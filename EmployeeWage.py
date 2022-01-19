2c633016f436cb3e67f8792d52f5aa69f661fdb0
Jay-R-H <jayhiremathr@gmail.com>
2022-01-19 19:16:21 +0530

    [jay]Added:Employee wage computation using switch-case

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -17,14 +17,20 @@ full_day_workingHrs = 8
 
 employee_status = randint(0,2)
 
-if employee_status == 0:
+def employeeCheckFunc(Status):
+    workingHrs = {
+        1 : 8,
+        2 : 4
+    }
+    return workingHrs.get(Status,0)
+
+
+if employeeCheckFunc(employee_status) == 0:
     print("Employee is absent")
-elif employee_status == 1:
+elif employeeCheckFunc(employee_status) == 1:
     print("Employee is present and works full time")
-    workingHrs = 8
 else:
     print("Employee is present and works part time")
-    workingHrs = 4
 
-wage = workingHrs * wage_per_hour
+wage = employeeCheckFunc(employee_status) * wage_per_hour
 print("Wage earned =",wage)
