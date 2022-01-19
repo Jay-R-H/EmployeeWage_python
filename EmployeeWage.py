4e2b8686c479b31d7845df1340f5d3cfd0a70b09
Jay-R-H <jayhiremathr@gmail.com>
2022-01-19 19:27:45 +0530

    [jay]Added:EmpWage calculation for a month

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -14,23 +14,32 @@ from random import randint
 
 wage_per_hour = 20
 full_day_workingHrs = 8
+max_working_days = 20
+worked_days = 0
+total_wage = 0
 
-employee_status = randint(0,2)
+while worked_days <= max_working_days: 
+    
+    employee_status = randint(0,2)
 
-def employeeCheckFunc(Status):
-    workingHrs = {
+    def employeeCheckFunc(Status):
+        workingHrs = {
         1 : 8,
         2 : 4
-    }
-    return workingHrs.get(Status,0)
+        }
+        return workingHrs.get(Status,0)
 
 
-if employeeCheckFunc(employee_status) == 0:
-    print("Employee is absent")
-elif employeeCheckFunc(employee_status) == 1:
-    print("Employee is present and works full time")
-else:
-    print("Employee is present and works part time")
+    if employeeCheckFunc(employee_status) == 0:
+        print("Employee is absent")
+    elif employeeCheckFunc(employee_status) == 1:
+        print("Employee is present and works full time")
+        worked_days+=1
+    else:
+        print("Employee is present and works part time")
+        worked_days+=1
 
-wage = employeeCheckFunc(employee_status) * wage_per_hour
-print("Wage earned =",wage)
+    wage = employeeCheckFunc(employee_status) * wage_per_hour
+    total_wage += wage
+
+print("Total Wage earned =",total_wage)
