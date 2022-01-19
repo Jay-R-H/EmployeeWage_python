d8ebd593952a43c1edb07938fecd05955a6ef89d
Jay-R-H <jayhiremathr@gmail.com>
2022-01-19 11:21:31 +0530

    [jay]Added:Employee wage computation

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -7,11 +7,13 @@
 
 @Last Modified time: 19-01-2022
 
-@Title : Employee Attendance status check
+@Title : Employee daily wage computation.
 '''
 
 from random import randint
 
+wage_per_hour = 20
+full_day_workingHrs = 8
 
 employee_status = randint(0,1)
 
@@ -19,3 +21,5 @@ if employee_status == 0:
     print("Employee is absent")
 else:
     print("Employee is present")
+    wage = full_day_workingHrs * wage_per_hour
+print("Wage earned =",wage)
