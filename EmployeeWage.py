3cb032ff7c120f7ef077fea398b7121852387fd3
Jay-R-H <jayhiremathr@gmail.com>
2022-01-19 18:58:39 +0530

    [jay]Added:Part time or full time to compute wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -15,11 +15,16 @@ from random import randint
 wage_per_hour = 20
 full_day_workingHrs = 8
 
-employee_status = randint(0,1)
+employee_status = randint(0,2)
 
 if employee_status == 0:
     print("Employee is absent")
+elif employee_status == 1:
+    print("Employee is present and works full time")
+    workingHrs = 8
 else:
-    print("Employee is present")
-    wage = full_day_workingHrs * wage_per_hour
+    print("Employee is present and works part time")
+    workingHrs = 4
+
+wage = workingHrs * wage_per_hour
 print("Wage earned =",wage)
