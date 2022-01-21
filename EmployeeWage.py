07d46d6f6d164aed04b1a68b062f56a6ecbe6345
Jay-R-H <jayhiremathr@gmail.com>
2022-01-21 12:37:25 +0530

    [jay]Refactored:The check func

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -18,26 +18,26 @@ max_working_days = 20
 worked_days = 0
 total_wage = 0
 
-while worked_days <= max_working_days: 
-    
-    employee_status = randint(0,2)
-
-    def employeeCheckFunc(Status):
+def employeeCheckFunc(Status):
         workingHrs = {
         1 : 8,
         2 : 4
         }
         return workingHrs.get(Status,0)
 
-
+while worked_days <= max_working_days: 
+    
+    employee_status = randint(0,2)
+    
     if employeeCheckFunc(employee_status) == 0:
         print("Employee is absent")
-    elif employeeCheckFunc(employee_status) == 1:
-        print("Employee is present and works full time")
-        worked_days+=1
     else:
-        print("Employee is present and works part time")
-        worked_days+=1
+        if employeeCheckFunc(employee_status) == 1:
+            print("Employee is present and works full time")
+            worked_days+=1
+        else:
+            print("Employee is present and works part time")
+            worked_days+=1
 
     wage = employeeCheckFunc(employee_status) * wage_per_hour
     total_wage += wage
